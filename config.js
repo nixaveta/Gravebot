670e466db99af0ba5157171595b4da8ff4b58f63
nixaveta <31187084+nixaveta@users.noreply.github.com>
2017-08-22 03:20:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -16,7 +16,7 @@ module.exports = {
   REDIS_URL: '',
 
   // Time to wait between commands per user in seconds. Default is 1.
-  MESSAGE_TTL: '',
+  MESSAGE_TTL: '0',
 
   // Youtube API key, used for searching videos with `!youtube`.
   // Learn more here. https://developers.google.com/youtube/v3/getting-started
@@ -24,12 +24,12 @@ module.exports = {
 
   // Imgflip username and password, used for creating memes with `!meme`.
   // Create an account here. https://imgflip.com
-  IMGFLIP_USERNAME: '',
-  IMGFLIP_PASSWORD: '',
+  IMGFLIP_USERNAME: 'feunix',
+  IMGFLIP_PASSWORD: 'y153m534',
 
   // Champion.gg API key, used for the League of Legends `!lol` commands.
   // Create a key here. http://api.champion.gg
-  CHAMPIONGG_KEY: '',
+  CHAMPIONGG_KEY: '6b35ecb40e345ffcf7fd5a91f1e038d9',
 
   // Riot's API key, used for player and match search commands.
   // BE WARNED, a development key will be very limited and most of the time will not work,
@@ -42,7 +42,7 @@ module.exports = {
 
   // PasteBin API key, used to create pastes with `!paste`.
   // Get a key here. http://pastebin.com/api
-  PASTEBIN_KEY: '',
+  PASTEBIN_KEY: 'd116f7104e38f28853d9a5e3eeb5be0f',
 
   // Popkey API key, used for searching GIFs with `!popkey`.
   // Apply for a key here. http://popkey.co/api/submit
@@ -50,16 +50,16 @@ module.exports = {
 
   // Random.org API key, used for getting random numbers and strings with `!random`.
   // Get a key here. https://api.random.org/api-keys
-  RANDOM_KEY: '',
+  RANDOM_KEY: 'faea5011-63f8-4e13-bfbe-5ec2a341785f',
 
   // Cleverbot API key, used for talking with `!chat`.
   // Get a key here. https://www.cleverbot.com/api
-  CLEVERBOT_KEY: '',
+  CLEVERBOT_KEY: 'CC42zfCKV6Hpt0n7wBhDd9ihAow',
 
   // Carbon and Discord Bots key, used for displaying server count on specific websites.
   // More information here. https://www.carbonitex.net/discord/bots & https://bots.discord.pw/api
-  CARBON_KEY: '',
-  DBOTS_KEY: '',
+  CARBON_KEY: 'DZMsfaxEEo8II4goLieVKefT64vthH&guild_id=312134465231257600',
+  DBOTS_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySUQiOiIxODI1Nzk0Mzk0MDg5MDYyNDAiLCJyYW5kIjo5MCwiaWF0IjoxNTAzMzQ2NzE3fQ.dnZqrhfV83eY71SWqjwSy7BzsPc-J3o79cxIrMtUZa4',
 
   // Feedback Channel ID, used for storing feedback with `!feedback`.
   FEEDBACK_CHANNEL_ID: ''
